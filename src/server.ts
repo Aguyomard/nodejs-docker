24a77d317e76aa8f7a640ed4c72c836954ed1d7b
alec guyomard <alecguyomard@yahoo.fr>
2025-02-09 18:32:41 +0100

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express'
+// import postRoutes from './routes/post.routes'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req: Request, res: Response) => {
   res.json({ result: 100 + 100 })
 })
+
+// app.use('post', postRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
