45ac88cfc5e222f0b72d93172ebeae064e2d2c22
alec guyomard <alecguyomard@yahoo.fr>
2025-02-09 14:58:41 +0100

    eslint and prettier added

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
+import express from 'express'
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.json({ result: 20 + 10 });
-});
+app.get('/', (req, res) => {
+  res.json({ result: 1 + 100 })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
