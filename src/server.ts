5ac8efe1055cce04fc509b0f2928ee7574b62096
alec guyomard <alecguyomard@yahoo.fr>
2025-02-09 16:31:18 +0100

    using ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.json({ result: 1 + 100 })
+app.get('/', (req: Request, res: Response) => {
+  res.json({ result: 10 + 100 })
 })
 
 app.listen(PORT, () => {
