9bc9fbf3677ddd413f3c0cbbb6753ce9536c3da8
alec guyomard <alecguyomard@yahoo.fr>
2025-02-23 15:54:37 +0100

    mongo connection

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
   res.json({ message: 'ok' })
 })
 
+router.post('/', (req, res) => {
+  res.json({ message: req.body.test })
+})
+
+router.put('/:id', (req, res) => {
+  res.json({ message: req.params.id })
+})
+
+router.delete('/:id', (req, res) => {
+  res.json({ message: req.params.id })
+})
+
+router.patch('/:id', (req, res) => {
+  res.json({ message: req.params.id })
+})
+
 export default router
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
 import express, { Request, Response } from 'express'
 import postRoutes from './routes/post.routes.js'
+import mongoose from 'mongoose'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
+const mongoURI = process.env.MONGO_URI || 'mongodb://mongo:27017/mydatabase'
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as any)
+  .then(() => console.log('✅ MongoDB connecté avec succès !'))
+  .catch((err: any) => console.error('❌ Erreur de connexion à MongoDB :', err))
+
+app.get('/test', (req: Request, res: Response) => {
   res.json({ result: 100 + 100 })
 })
 
-app.use('/get', postRoutes)
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/post', postRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
