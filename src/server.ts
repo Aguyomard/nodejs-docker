58df751b178fcccf0ed02b40e510f0eb9f042a29
alec guyomard <alecguyomard@yahoo.fr>
2025-02-24 13:51:16 +0100

    tsx + npm ci

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,23 @@ dotenv.config()
 import express, { Request, Response } from 'express'
 import postRoutes from './routes/post.routes.js'
 import connectDB from './config/db.js'
+import cors from 'cors'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 
 // Middleware
+app.use(cors())
+app.use(helmet())
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.get('/test', (req: Request, res: Response) => {
-  res.json({ result: 100 + 100 })
+  res.json({ result: 100 + 1 })
 })
 
 app.use('/post', postRoutes)
