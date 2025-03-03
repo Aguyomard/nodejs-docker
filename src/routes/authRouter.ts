ccc4bf57907fdc7faee2a355a97aa6a594cfb0d2
alec guyomard <alecguyomard@yahoo.fr>
2025-03-03 12:30:32 +0100

    mail controller

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import { AuthController } from '../controller/authController.js'
+import { MailController } from '../controller/MailController.js'
+import { mailService } from '../config/dependencies.js'
 
 const router = express.Router()
+
+const mailController = new MailController(mailService)
 
 router.post('/signup', (req, res) => AuthController.signup(req, res))
 router.get('/signin', (req, res) => AuthController.signin(req, res))
 router.get('/signout', (req, res) => AuthController.signin(req, res))
 router.get('/mail', (req, res) => AuthController.mail(req, res))
+router.get('/send-email', mailController.sendEmail)
 
 export default router
