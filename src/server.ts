5c43b2097cd405c50f0b486bfe3a89383a110dfd
alec guyomard <alecguyomard@yahoo.fr>
2025-03-08 12:26:54 +0100

    uncaught exception management

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,31 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import express, { Request, Response } from 'express'
-import postRoutes from './routes/post.routes.js'
-import authRouter from './routes/authRouter.js'
-
-import connectDB from './config/db.js'
+import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
+
+import postRoutes from './routes/post.routes.js'
+import authRouter from './routes/authRouter.js'
+import connectDB from './config/db.js'
 import { errorHandler } from './middlewares/errorHandler.js'
+
+// Gestion des erreurs globales
+process.on('uncaughtException', (err) => {
+  console.error('❌ Uncaught Exception:', err)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('❌ Unhandled Rejection at:', promise, 'reason:', reason)
+  process.exit(1)
+})
 
 const app = express()
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 
-// Middleware
+// Middlewares
 app.use(cors())
 app.use(helmet())
 app.use(cookieParser())
@@ -22,26 +33,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // Routes
-app.get('/test', (req: Request, res: Response) => {
-  res.json({ result: 100 + 1 })
+app.get('/test', (req, res) => {
+  res.json({ result: 101 })
 })
 
 app.use('/post', postRoutes)
 app.use('/api/auth', authRouter)
 
-// Fonction pour démarrer le serveur après la connexion à MongoDB
+// Gestion des erreurs
+app.use(errorHandler)
+
 const startServer = async () => {
   try {
-    await connectDB() // Attendre la connexion à la base de données
+    await connectDB()
     app.listen(PORT, () => {
       console.log(`🚀 Server is running on http://localhost:${PORT}`)
     })
   } catch (error) {
     console.error('❌ Échec du démarrage du serveur :', error)
-    process.exit(1) // Quitte le processus en cas d'échec
+    process.exit(1)
   }
 }
 
-app.use(errorHandler)
-
-startServer() // Démarrer l'application proprement
+startServer()
